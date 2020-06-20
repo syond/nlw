e231ac5ee9eb74d5e808c589b66247d2e67792cc
Syond <syond94@gmail.com>
2020-06-20 14:34:22 -0300

    Set Multer to create Point router

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,7 +3,12 @@ import express from 'express';
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+
 const routes = express.Router();
+const upload = multer(multerConfig);
 
 const pointsController  = new PointsController();
 const itemsController   = new ItemsController();
@@ -12,7 +17,7 @@ routes.get('/items', itemsController.index);
 
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
-routes.post('/points', pointsController.create);
+routes.post('/points', upload.single('image'), pointsController.create);
 
 
 export default routes;
