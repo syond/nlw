c929365f533e33cc1f9940649adce3ab38df2c43
Syond <syond94@gmail.com>
2020-06-20 23:41:56 -0300

    add image_url to interfaces

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -14,6 +14,7 @@ interface Params {
 interface Data {
   point: {
     image: string,
+    image_url: string;
     name: string,
     email: string,
     whatsapp: number,
@@ -67,7 +68,7 @@ export default function Detail() {
           <Icon name="arrow-left" size={20} color={"#34cb79"}></Icon>
         </TouchableOpacity>
 
-        <Image style={styles.pointImage} source={{ uri: data.point.image }} />
+        <Image style={styles.pointImage} source={{ uri: data.point.image_url }} />
 
         <Text style={styles.pointName}>{data.point.name}</Text>
         <Text style={styles.pointItems}>{data.items.map(item => item.title).join(', ')}</Text>
--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -17,6 +17,7 @@ interface Point{
   id: number;
   name: string;
   image: string;
+  image_url: string;
   latitude: number;
   longitude: number;
 }
@@ -133,7 +134,7 @@ export default function Points() {
                       onPress={() => handleNavigateToDetail(point.id)}
                     >
                       <View style={styles.mapMarkerContainer}>
-                        <Image style={styles.mapMarkerImage} source={{ uri: point.image}} />
+                        <Image style={styles.mapMarkerImage} source={{ uri: point.image_url}} />
                         <Text style={styles.mapMarkerTitle}>{point.name}</Text>
                       </View>
                     </Marker>
